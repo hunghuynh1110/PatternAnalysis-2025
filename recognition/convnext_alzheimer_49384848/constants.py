23a7032c0d84ef95ba7fdcb512e91a3641197f31
1110giahung <144457830+1110giahung@users.noreply.github.com>
2025-10-21 18:45:31 +1000

    change LR, batch size

--- recognition/convnext_alzheimer_49384848/constants.py
+++ recognition/convnext_alzheimer_49384848/constants.py
@@ -13,14 +13,12 @@ DEVICE_CUDA = 'cuda'
 DEVICE_CPU  = 'cpu'
 
 # Data & training
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 EPOCHS     = 5
-LR         = 4e-3
+LR         = 2e-4
 WD         = 3e-4
-
-WD = 0.01
 
 DATA_ROOT = './recognition/convnext_alzheimer_49384848/AD_NC'
 
 NUM_CLASSES   = 2
-DROP_PATH_RATE = 0.2  # for example, maybe 0.1 for tiny model
+DROP_PATH_RATE = 0.25  # for example, maybe 0.1 for tiny model
